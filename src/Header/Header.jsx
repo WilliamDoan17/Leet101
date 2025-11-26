c9df88cfe6ee7d39c0ce5943641acb559c3169ef
WilliamDoan17 <pdoanthe96@gmail.com>
2025-11-26 15:57:44 -0500

    Added link to solutions

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -2,6 +2,8 @@ import styles from './Header.module.css';
 import { useState, useEffect } from 'react';
 
 const Header = ({ author, appName }) => {
+    const solutionsLink = 'https://williamdoan17.github.io/Leet101-Forum/';
+
     return (
         <div 
             className={styles.container}
@@ -12,6 +14,17 @@ const Header = ({ author, appName }) => {
                 <p className={`${styles['app-info']} ${styles['app-name']}`}>{appName}</p>
                 <p className={`${styles['app-info']} ${styles.author}`}>by {author}</p>
             </div>
+            <nav
+                className = {`${styles['navigation']}`}
+            >
+                <a 
+                    className = {`${styles['navigation-button']}`}
+                    href = {solutionsLink}
+                    target = '_blank'
+                >
+                    Solutions
+                </a>
+            </nav>
         </div>
     )
 }
